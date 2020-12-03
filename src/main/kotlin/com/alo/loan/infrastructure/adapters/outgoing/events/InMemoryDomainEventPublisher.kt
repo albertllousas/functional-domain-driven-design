c6f0b3edb9b4c99869f70a88c06347b0f83fb267
Albert Llousas <albert.llousas@n26.com>
2020-12-03 09:36:01 +0100

    Add content

--- src/main/kotlin/com/alo/loan/infrastructure/adapters/outgoing/events/InMemoryDomainEventPublisher.kt
+++ src/main/kotlin/com/alo/loan/infrastructure/adapters/outgoing/events/InMemoryDomainEventPublisher.kt
@@ -5,6 +5,7 @@ import com.alo.loan.domain.model.PublishEvents
 
 interface DomainEventSubscriber : (DomainEvent) -> Unit
 
+// in a real environment we would face here a dual write problem, having to apply transactional outbox pattern or other solution
 class InMemoryDomainEventPublisher(private val subscribers: List<DomainEventSubscriber>) : PublishEvents {
     override fun invoke(domainEvents: List<DomainEvent>) {
         subscribers.forEach { subscriber -> domainEvents.forEach(subscriber) }
